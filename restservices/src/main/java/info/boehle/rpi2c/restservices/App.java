2a313657bb1d4a41ce372a891735f8be7a2f6054
developer <developer@openHAB-Dev-VirtualBox>
2017-07-23 12:29:38 +0200

    initial version

---
+++ restservices/src/main/java/info/boehle/rpi2c/restservices/App.java
@@ -0,0 +1,13 @@
+package info.boehle.rpi2c.restservices;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
