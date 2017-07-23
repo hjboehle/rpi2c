577d6c993474a18abff58ef5628409123517ad73
Hans Jürgen Böhle <web@boehle.info>
2017-07-23 16:20:15 +0200

    hello world version

--- restservices/src/main/java/info/boehle/rpi2c/restservices/App.java
+++ restservices/src/main/java/info/boehle/rpi2c/restservices/App.java
@@ -1,13 +1,40 @@
 package info.boehle.rpi2c.restservices;
 
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+	public static void main(String[] args) {
+	
+	    ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        context.setContextPath("/");
+
+        Server jettyServer = new Server(2222);
+        jettyServer.setHandler(context);
+
+        ServletHolder jerseyServlet = context.addServlet(
+             org.glassfish.jersey.servlet.ServletContainer.class, "/*");
+        jerseyServlet.setInitOrder(0);
+
+        // Tells the Jersey Servlet which REST service/class to load.
+        jerseyServlet.setInitParameter(
+           "jersey.config.server.provider.classnames",
+           Resource.class.getCanonicalName());
+		
+		try {
+			jettyServer.start();
+			jettyServer.join();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			jettyServer.destroy();
+		}
+	}
+
 }
